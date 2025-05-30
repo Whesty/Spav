25b0e68e6a230fd3d106d0545cd8ac668499c17d
Диана Мамаева <mamaeva.diana.a@gmail.com>
2025-05-30 15:50:14 +0300

    bd

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: "postgresql://neondb_owner:npg_0vih5KTcUOCl@ep-red-wildflower-a8oh1588-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
 });
 
 console.log('Connecting to DB:', {
