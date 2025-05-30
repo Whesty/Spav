6213d586fffae3fe26c89a939f6ee6b513e2c7d1
Диана Мамаева <mamaeva.diana.a@gmail.com>
2025-05-30 16:52:59 +0300

    Fix build v1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,22 +9,8 @@ import path from 'node:path'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    assetsInlineLimit: 4096, // 4kb, можно изменить или отключить
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
-    },
-  },
-});
+  resolve: { alias: { '@': path.resolve(__dirname, './src') }},
+  base: '/',  // <-- изменить здесь
+  build: { outDir: 'dist' }
+})
+
