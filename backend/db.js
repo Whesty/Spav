f96c06f3009faac4d69008fa0cae7da2a7c7d596
Диана Мамаева <mamaeva.diana.a@gmail.com>
2025-05-30 16:09:58 +0300

    bd

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    DATABASE_URL: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    }
 });
 
 console.log('Connecting to DB:', {
